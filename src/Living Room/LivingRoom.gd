7d50834e90925e0a898792d80358019bffdf3c3b
Ishlah Muhammadi <ishlahims@gmail.com>
2022-02-06 22:04:24 +0700

    yok semangat

--- src/Living Room/LivingRoom.gd
+++ src/Living Room/LivingRoom.gd
@@ -1,10 +1,12 @@
 extends Node2D
 
 onready var anim = $transition/AnimationPlayer
+onready var theBook = $"The Book"
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	anim.play("fade_out")
+	theBook.hide()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -18,6 +20,39 @@ func _on_Area2D_mouse_entered():
 func _on_Area2D_mouse_exited():
 	Input.set_default_cursor_shape(Input.CURSOR_ARROW)
 
+func _on_Drawer_input_event(viewport, event, shape_idx):
+	if event is InputEventMouseButton:
+		if event.is_pressed():
+			Global.target_location = "res://src/Living Room/Drawer.tscn"
+			$"transition/ColorRect".show()
+			anim.play("fade_in")
 
 func _on_Book_input_event(viewport, event, shape_idx):
-	pass # Replace with function body.
+	var theBookAnim = $"The Book/TheBookAnimation"
+	if event is InputEventMouseButton:
+		if event.is_pressed():
+			theBook.show()
+			theBookAnim.play("move_in")
+
+
+func _on_The_Book_input_event(viewport, event, shape_idx):
+	var theBookAnim = $"The Book/TheBookAnimation"
+	if event is InputEventMouseButton:
+		if event.is_pressed():
+			theBookAnim.play("move_out")
+
+func _on_TheBookAnimation_animation_finished(anim_name):
+	if anim_name == "move_in":
+		$Book.hide()
+		$Piano.hide()
+		$Clock.hide()
+		$Door.hide()
+		$Drawer.hide()
+	elif anim_name == "move_out":
+		theBook.hide()
+		$Book.show()
+		$Piano.show()
+		$Clock.show()
+		$Door.show()
+		$Drawer.show()
+
--- src/global.gd
+++ src/global.gd
@@ -4,6 +4,8 @@ var inventory = []
 var from_location = ""
 var target_location = ""
 
+var drawerCond = "res://assets/images/items/Level 5-lemari/lemari1 (ada kunci).png"
+
 onready var select_sound = $select
 
 func _ready():
