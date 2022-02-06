b89b3803fd8f048c5bf76a2e7e79a6f08e2f4a1f
Ishlah Muhammadi <ishlahims@gmail.com>
2022-02-06 19:33:07 +0700

    Ntahlah apa ini

--- src/Living Room/LivingRoom.gd
+++ src/Living Room/LivingRoom.gd
@@ -10,3 +10,10 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+func _on_Area2D_mouse_entered():
+	Input.set_default_cursor_shape(Input.CURSOR_POINTING_HAND)
+
+
+func _on_Area2D_mouse_exited():
+	Input.set_default_cursor_shape(Input.CURSOR_ARROW)
