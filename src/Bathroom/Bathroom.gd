1947e93a19de23c601f8eea2225fc1478550e7f0
Andreas Indra Kurniawan <andreasindrakurniawan@gmail.com>
2022-02-06 22:30:29 +0700

    istri marah deadline mepet

---
+++ src/Bathroom/Bathroom.gd
@@ -0,0 +1,30 @@
+extends Node2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	var t
+	t = $Timer
+	t.set_one_shot(true)
+	t.start()
+	
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_Timer_timeout():
+	var Door_sound = $AudioStreamPlayer2D
+	Door_sound.play()
+	var jumpscare_sound = $NoniDeadEnd/AudioStreamPlayer2D
+	jumpscare_sound.play()
+	$NoniDeadEnd/NoniThird.show()
+	pass # Replace with function body.
